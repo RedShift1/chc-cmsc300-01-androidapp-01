e4c527d83416fb809e59bedaad3970aabf1e0764
Dylan Reimer <reimerd@chc.edu>
2015-11-03 14:12:02 -0500

    Floating action button is working, and list of courses is getting closer to the mockup.

--- app/src/main/java/edu/chc/appdev/teama/gradekeeper/MainActivity.java
+++ app/src/main/java/edu/chc/appdev/teama/gradekeeper/MainActivity.java
@@ -2,6 +2,8 @@ package edu.chc.appdev.teama.gradekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +13,14 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] myStringArray={"A","B","C"};
+        ArrayAdapter<String> myAdapter=new
+                ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                myStringArray);
+        ListView myList=
+                (ListView) findViewById(R.id.courseListView);
+        myList.setAdapter(myAdapter);
     }
 }
