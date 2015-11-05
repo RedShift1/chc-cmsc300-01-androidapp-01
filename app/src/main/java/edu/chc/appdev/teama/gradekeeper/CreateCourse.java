a68531d2e0368e23a46aa142c385dbd94c6f5510
Glenn Matthys <glenn@webmind.be>
2015-11-04 20:01:35 -0500

    Create CreateCourse activity

--- app/src/main/java/edu/chc/appdev/teama/gradekeeper/CreateCourse.java
+++ app/src/main/java/edu/chc/appdev/teama/gradekeeper/CreateCourse.java
@@ -2,6 +2,11 @@ package edu.chc.appdev.teama.gradekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import edu.chc.appdev.teama.gradekeeper.DB.DB;
 
 public class CreateCourse extends AppCompatActivity
 {
@@ -11,5 +16,20 @@ public class CreateCourse extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_course);
+    }
+
+    public void addCourseToDb(View view)
+    {
+        DB db = new DB(this, null, null);
+
+        db.addCourse(
+            ((EditText) this.findViewById(R.id.txtName)).getText().toString(),
+            ((EditText) this.findViewById(R.id.txtCode)).getText().toString(),
+            ((EditText) this.findViewById(R.id.txtDescription)).getText().toString()
+        );
+
+        (Toast.makeText(this, "Added!", Toast.LENGTH_LONG)).show();
+
+        this.finish();
     }
 }
