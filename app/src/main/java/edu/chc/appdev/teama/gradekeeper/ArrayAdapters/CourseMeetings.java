5b89dd50639bfc97fa0704520bcdc716045ba6b9
Glenn Matthys <glenn@webmind.be>
2015-11-05 11:37:31 -0500

    Add Utils class and CourseMeetings ArrayAdapter

---
+++ app/src/main/java/edu/chc/appdev/teama/gradekeeper/ArrayAdapters/CourseMeetings.java
@@ -0,0 +1,8 @@
+package edu.chc.appdev.teama.gradekeeper.ArrayAdapters;
+
+/**
+ * Created by Glenn on 5/11/2015.
+ */
+public class CourseMeetings
+{
+}
---
+++ app/src/main/java/edu/chc/appdev/teama/gradekeeper/Utils.java
@@ -0,0 +1,8 @@
+package edu.chc.appdev.teama.gradekeeper;
+
+/**
+ * Created by Glenn on 5/11/2015.
+ */
+public class Utils
+{
+}
