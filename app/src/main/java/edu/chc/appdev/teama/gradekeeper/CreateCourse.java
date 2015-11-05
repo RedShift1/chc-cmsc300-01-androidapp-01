3a77fe701d8275b6f3671934acd7a5a5d24dff12
Glenn Matthys <glenn@webmind.be>
2015-11-05 08:46:48 -0500

    Add slideDayTimePicker fragment

--- app/src/main/java/edu/chc/appdev/teama/gradekeeper/CreateCourse.java
+++ app/src/main/java/edu/chc/appdev/teama/gradekeeper/CreateCourse.java
@@ -5,13 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.github.jjobes.slidedaytimepicker.SlideDayTimeListener;
+import com.github.jjobes.slidedaytimepicker.SlideDayTimePicker;
 
 import edu.chc.appdev.teama.gradekeeper.DB.DB;
 
 public class CreateCourse extends AppCompatActivity
 {
+
+    private SlideDayTimeListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,6 +27,23 @@ public class CreateCourse extends AppCompatActivity
 
         this.setTitle("Add course");
         (this.getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        listener = new SlideDayTimeListener() {
+
+            @Override
+            public void onDayTimeSet(int day, int hour, int minute)
+            {
+                // Do something with the day, hour and minute
+                // the user has selected.
+            }
+
+            @Override
+            public void onDayTimeCancel()
+            {
+                // The user has canceled the dialog.
+                // This override is optional.
+            }
+        };
     }
 
     @Override
@@ -41,6 +64,21 @@ public class CreateCourse extends AppCompatActivity
             default:
                 return true;
         }
+    }
+
+    public void showDateTimePicker(View view)
+    {
+        new SlideDayTimePicker.Builder(getSupportFragmentManager())
+                .setListener(this.listener)
+                .setInitialDay(1)
+                .setInitialHour(13)
+                .setInitialMinute(30)
+                .setIs24HourTime(true)
+                        //.setCustomDaysArray(getResources().getStringArray(R.array.days_of_week))
+                        //.setTheme(SlideDayTimePicker.HOLO_DARK)
+                        //.setIndicatorColor(Color.parseColor("#990000"))
+                .build()
+                .show();
     }
 
     public void addCourseToDb(MenuItem menuItem)
