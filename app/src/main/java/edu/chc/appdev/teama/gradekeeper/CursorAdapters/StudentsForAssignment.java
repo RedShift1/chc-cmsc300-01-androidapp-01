4f24a481fe0bd3d525df6e28503136d4e0151ed5
Glenn Matthys <glenn@webmind.be>
2015-11-11 20:28:49 -0500

    Add StudentsForAssignment CursorAdapter

---
+++ app/src/main/java/edu/chc/appdev/teama/gradekeeper/CursorAdapters/StudentsForAssignment.java
@@ -0,0 +1,37 @@
+package edu.chc.appdev.teama.gradekeeper.CursorAdapters;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import edu.chc.appdev.teama.gradekeeper.R;
+
+/**
+ * Created by Glenn on 11/11/2015.
+ */
+public class StudentsForAssignment extends CursorAdapter
+{
+
+    public StudentsForAssignment(Context context, Cursor c, int flags)
+    {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent)
+    {
+        return LayoutInflater.from(context).inflate(R.layout.li_gradeassignment_student, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor)
+    {
+        ((TextView) view.findViewById(R.id.li_student_name)).
+                setText(cursor.getString(cursor.getColumnIndex("name")));
+    }
+}
