b0b69ebcfb07b1b8278dde265232131c6c142dac
Glenn Matthys <glenn@webmind.be>
2015-11-11 21:00:17 -0500

    Add buttons to ActionBar in GradeAssignmentActivity

--- app/src/main/java/edu/chc/appdev/teama/gradekeeper/GradeAssignmentActivity.java
+++ app/src/main/java/edu/chc/appdev/teama/gradekeeper/GradeAssignmentActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
@@ -54,6 +55,20 @@ public class GradeAssignmentActivity extends AppCompatActivity
             Log.w("Gradekeeper", "No assignment found with ID " + this.id);
         }
     }
+
+    public void deleteAssigmentFromDB(MenuItem menuItem)
+    {
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        this.getMenuInflater().inflate(R.menu.gradeassignmentactivity, menu);
+        return true;
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
