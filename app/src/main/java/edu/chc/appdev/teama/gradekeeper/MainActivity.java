1a1503eaf4456046722023568dcbb2dfb2b8df62
Glenn Matthys <glenn@webmind.be>
2015-11-28 15:31:59 -0500

    Clean up imports in MainActivity

--- app/src/main/java/edu/chc/appdev/teama/gradekeeper/MainActivity.java
+++ app/src/main/java/edu/chc/appdev/teama/gradekeeper/MainActivity.java
@@ -2,8 +2,6 @@ package edu.chc.appdev.teama.gradekeeper;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
