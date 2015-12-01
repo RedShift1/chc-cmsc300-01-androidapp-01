a9de0ba1184980919c36a9dff341c4429ff8b4dd
Glenn Matthys <glenn@webmind.be>
2015-11-30 19:43:48 -0500

    Rename StudentActivity to ViewStudentActivity

--- app/src/main/java/edu/chc/appdev/teama/gradekeeper/ViewStudentActivity.java
+++ app/src/main/java/edu/chc/appdev/teama/gradekeeper/ViewStudentActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class StudentActivity extends AppCompatActivity
+public class ViewStudentActivity extends AppCompatActivity
 {
 
     @Override
