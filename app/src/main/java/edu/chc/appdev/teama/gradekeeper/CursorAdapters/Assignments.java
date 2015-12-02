57391a7fdaa13a1cca30473f6064b7e4c98825a4
Glenn Matthys <glenn@webmind.be>
2015-12-02 18:53:34 -0500

    Generate color icon in Assignments

--- app/src/main/java/edu/chc/appdev/teama/gradekeeper/CursorAdapters/Assignments.java
+++ app/src/main/java/edu/chc/appdev/teama/gradekeeper/CursorAdapters/Assignments.java
@@ -6,7 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -20,9 +24,12 @@ import edu.chc.appdev.teama.gradekeeper.R;
 public class Assignments extends CursorAdapter
 {
 
+    ColorGenerator colorGen;
+
     public Assignments(Context context, Cursor c, int flags)
     {
         super(context, c, flags);
+        this.colorGen = ColorGenerator.MATERIAL;
     }
 
     @Override
@@ -37,11 +44,22 @@ public class Assignments extends CursorAdapter
         SimpleDateFormat sdf = new SimpleDateFormat("EEE, MMM d, y", Locale.getDefault());
         String dueDate = sdf.format(new Date(cursor.getLong(cursor.getColumnIndex("duedate"))));
 
+        String assignmentName = cursor.getString(cursor.getColumnIndex("name"));
+
         ((TextView) view.findViewById(R.id.li_assignment_name)).
-                setText(cursor.getString(cursor.getColumnIndex("name")));
+                setText(assignmentName);
         ((TextView) view.findViewById(R.id.li_assignment_duedate)).
                 setText(dueDate);
         ((TextView) view.findViewById(R.id.li_assignment_maxgrade)).
                 setText(cursor.getString(cursor.getColumnIndex("maxgrade")));
+
+        if(assignmentName.length() > 0)
+        {
+            TextDrawable drawable = TextDrawable.builder()
+                .buildRound(assignmentName.substring(0, 1).toUpperCase(), this.colorGen.getColor(assignmentName));
+
+            ImageView image = (ImageView) view.findViewById(R.id.iv_name_icon);
+            image.setImageDrawable(drawable);
+        }
     }
 }
