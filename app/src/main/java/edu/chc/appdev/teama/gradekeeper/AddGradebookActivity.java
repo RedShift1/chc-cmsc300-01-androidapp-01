20a1b6d176564d142a52ef9f927946d97f5f7b89
Glenn Matthys <glenn@webmind.be>
2015-12-02 20:17:10 -0500

    Implement meeting times in AddGradebookActivity

--- app/src/main/java/edu/chc/appdev/teama/gradekeeper/AddGradebookActivity.java
+++ app/src/main/java/edu/chc/appdev/teama/gradekeeper/AddGradebookActivity.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.github.jjobes.slidedaytimepicker.SlideDayTimeListener;
+import com.github.jjobes.slidedaytimepicker.SlideDayTimePicker;
+
+import java.util.ArrayList;
+
+import edu.chc.appdev.teama.gradekeeper.ArrayAdapters.CourseMeetings;
 import edu.chc.appdev.teama.gradekeeper.DB.DB;
 import edu.chc.appdev.teama.gradekeeper.FormValidator.EditTextValidator;
 import edu.chc.appdev.teama.gradekeeper.FormValidator.FormValidator;
@@ -20,6 +28,14 @@ public class AddGradebookActivity extends AppCompatActivity
     private long id;
 
     private FormValidator validator;
+    private SlideDayTimeListener listener;
+    private ArrayList<String[]> meetingTimes;
+    private CourseMeetings meetingTimesAdapter;
+
+    public AddGradebookActivity()
+    {
+        this.meetingTimes = new ArrayList<>();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -42,11 +58,43 @@ public class AddGradebookActivity extends AppCompatActivity
         this.validator.addField(
             new EditTextValidator(
                 (EditText) this.findViewById(R.id.etName),
-                new ITextValidator[] { new NotEmpty() }
+                new ITextValidator[]{new NotEmpty()}
             )
         );
+
+
+        this.listener = new SlideDayTimeListener() {
+
+            @Override
+            public void onDayTimeSet(int day, int hour, int minute)
+            {
+                AddGradebookActivity.this.addMeetingTime(day, hour, minute);
+            }
+        };
+
+        this.meetingTimesAdapter = new CourseMeetings(this, this.meetingTimes);
+        ((ListView) this.findViewById(R.id.lvMeetings)).setAdapter(this.meetingTimesAdapter);
     }
 
+    public void addMeetingTime(int day, int hour, int minute)
+    {
+        String[] item = {String.valueOf(day), hour + ":" + minute};
+        this.meetingTimes.add(item);
+        this.meetingTimesAdapter.notifyDataSetChanged();
+    }
+
+
+    public void showDateTimePicker(View view)
+    {
+        new SlideDayTimePicker.Builder(this.getSupportFragmentManager())
+            .setListener(this.listener)
+            .setInitialDay(1)
+            .setInitialHour(13)
+            .setInitialMinute(30)
+            .setIs24HourTime(true)
+            .build()
+            .show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
